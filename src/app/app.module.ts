a5e58bd8be74ea834cc3764648e450fc1eb841d1
EduardMarinho237 <edu.docxl@gmail.com>
2024-11-30 18:58:46 -0300

    Adiciona a função de historico baseado no clique do botão na tela Home (salvando no localStorage), funções de apagar transações individualmente ou todas juntas também foram implementadas (com leves animações)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';  
+
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,6 +18,8 @@ import { AppRoutingModule } from './app-routing.module';
     IonicModule.forRoot(),
     AppRoutingModule,
     HttpClientModule,
+    BrowserModule, 
+    BrowserAnimationsModule
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
-import { CurrencyService } from '../services/currency.service';
+import { ViewWillLeave } from '@ionic/angular';  
+import { CurrencyService } from '../services/currency.service';  
+
+interface ExchangeRatesResponse {
+  conversion_rates: { [key: string]: number };
+  base: string;
+  date: string;
+}
 
 @Component({
   selector: 'app-tab1',
   templateUrl: './tab1.page.html',
   styleUrls: ['./tab1.page.scss'],
 })
-export class Tab1Page {
-  
-  inputValue: number | null = null; 
-  formattedInputValue: string = '';  
+export class Tab1Page implements ViewWillLeave {
+
+  inputValue: number | null = null;
+  formattedInputValue: string = '';
   outputValue: number = 0;
   formattedOutputValue: string = '0,00';
 
@@ -17,58 +24,92 @@ export class Tab1Page {
   toCurrency: string = 'USD';
   exchangeRates: any = {};
 
+  transactionHistory: any[] = [];
+
   constructor(private currencyService: CurrencyService) {}
 
   ngOnInit() {
-    this.loadExchangeRates(this.fromCurrency); 
+    this.loadTransactionHistory();
+    this.loadExchangeRates(this.fromCurrency);
+  }
+
+  ionViewWillLeave() {
+    this.inputValue = null;
+    this.formattedInputValue = '';
+    this.outputValue = 0;
+    this.formattedOutputValue = '0,00';
   }
 
   loadExchangeRates(baseCurrency: string) {
-    this.currencyService.getExchangeRates(baseCurrency).subscribe((data) => {
+    this.currencyService.getExchangeRates(baseCurrency).subscribe((data: ExchangeRatesResponse) => {
       this.exchangeRates = data.conversion_rates;
-      this.convertCurrency(); 
+      this.convertCurrency();
     });
   }
 
   convertCurrency() {
     if (this.exchangeRates && this.exchangeRates[this.toCurrency]) {
       const rate = this.exchangeRates[this.toCurrency];
-      this.outputValue = this.inputValue && this.inputValue >= 0 ? this.inputValue * rate : 0;  
-      this.formattedOutputValue = this.formatCurrency(this.outputValue); 
+      this.outputValue = this.inputValue && this.inputValue >= 0 ? this.inputValue * rate : 0;
+      this.formattedOutputValue = this.formatCurrency(this.outputValue);
     }
   }
 
   formatCurrency(value: number): string {
-    return value.toFixed(2).replace('.', ','); 
+    return value.toFixed(2).replace('.', ',');
   }
 
   onInputChange(event: any) {
-    let input = event.target.value.replace(',', '.'); 
-    this.inputValue = parseFloat(input); 
- 
+    let input = event.target.value.replace(',', '.');
+    this.inputValue = parseFloat(input);
+
     if (this.inputValue < 0) {
       this.inputValue = null;
-      this.formattedInputValue = ''; 
+      this.formattedInputValue = '';
     } else {
-      this.formattedInputValue = this.formatCurrency(this.inputValue ?? 0); 
+      this.formattedInputValue = this.formatCurrency(this.inputValue ?? 0);
     }
 
-    this.convertCurrency(); 
+    this.convertCurrency();
   }
 
   onFromCurrencyChange() {
-    this.loadExchangeRates(this.fromCurrency); 
+    this.loadExchangeRates(this.fromCurrency);
   }
 
   onToCurrencyChange() {
-    this.convertCurrency(); 
+    this.convertCurrency();
   }
 
   onSwapCurrencies() {
     const temp = this.fromCurrency;
     this.fromCurrency = this.toCurrency;
     this.toCurrency = temp;
-
     this.loadExchangeRates(this.fromCurrency);
+  }
+
+  saveTransaction() {
+    if (this.inputValue !== null && this.inputValue >= 0) {
+      const transaction = {
+        inputValue: this.inputValue,
+        fromCurrency: this.fromCurrency,
+        toCurrency: this.toCurrency,
+        outputValue: this.outputValue,
+        formattedOutputValue: this.formattedOutputValue,
+        timestamp: new Date().toLocaleString(),
+      };
+
+      this.transactionHistory.push(transaction);
+      localStorage.setItem('transactionHistory', JSON.stringify(this.transactionHistory));
+    } else {
+      console.log('Erro: valores inválidos para salvar transação');
+    }
+  }
+
+  loadTransactionHistory() {
+    const savedHistory = localStorage.getItem('transactionHistory');
+    if (savedHistory) {
+      this.transactionHistory = JSON.parse(savedHistory);
+    }
   }
 }
